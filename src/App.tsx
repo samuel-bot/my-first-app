46f03155247bbd029f833ea45f4608e4570e6d63
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-17 09:16:16 -0300

    add component button

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import MyButton from "./componentes/MyButton";
+
 function App() {
   return (
     <>
       <h1>Hello World! React + Vite</h1>
-      <p>Bem-vindos</p>
+      <MyButton/>
     </>
   );
 }
---
+++ src/componentes/MyButton/index.tsx
@@ -0,0 +1,9 @@
+import './styles.css'
+
+export default function MyButton() {
+    return (
+        <button className="button-custom buttom-custom:hover">
+            Clique aqui
+        </button>
+    )
+}
